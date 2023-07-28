e1355453a08b99d0765abc814c911196d687f751
Maximiliano Zulli <if19b183@dev.azure.com>
2023-07-28 15:33:47 +0200

    initial commit with Radzen.Blazor Components in Blazor Project

---
+++ BlazorWebUi/IOTHubBlazor/Client/Program.cs
@@ -0,0 +1,25 @@
+using IOTHubBlazor.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+namespace IOTHubBlazor.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
